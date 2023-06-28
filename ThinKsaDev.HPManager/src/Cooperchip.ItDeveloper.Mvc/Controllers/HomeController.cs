bf23fd26fb4be0de8d3afc3b6ac0c51449dc086e
@thiagofcasa <@thiagofcasa>
2023-06-29 00:17:06 +0100

    Developing partial views part 1

---
+++ ThinKsaDev.HPManager/src/Cooperchip.ItDeveloper.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Cooperchip.ItDeveloper.Mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Cooperchip.ItDeveloper.Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
