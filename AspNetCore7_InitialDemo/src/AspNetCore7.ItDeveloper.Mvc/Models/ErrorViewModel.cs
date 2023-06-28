ffbd8a6bd15b2b28ab623f1ca69c7925c08e6a4d
@thiagofcasa <@thiagofcasa>
2023-06-28 23:03:17 +0100

    Preparing frontend template

---
+++ AspNetCore7_InitialDemo/src/AspNetCore7.ItDeveloper.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetCore7.ItDeveloper.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
